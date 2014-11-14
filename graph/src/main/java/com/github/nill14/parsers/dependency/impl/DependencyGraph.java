2cfe4fd86539392b0ab8824d9170ee0b42a9bf23
Pavel Sklenak <pavel.sklenak@gmail.com>
2014-11-14 01:24:52 +0100

    Create a new graph walker (possibly faster)

--- graph/src/main/java/com/github/nill14/parsers/dependency/impl/DependencyGraph.java
+++ graph/src/main/java/com/github/nill14/parsers/dependency/impl/DependencyGraph.java
@@ -3,17 +3,16 @@ package com.github.nill14.parsers.dependency.impl;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
-import com.github.nill14.parsers.dependency.IDependencyGraph;
 import com.github.nill14.parsers.dependency.IConsumer;
+import com.github.nill14.parsers.dependency.IDependencyGraph;
 import com.github.nill14.parsers.graph.CyclicGraphException;
 import com.github.nill14.parsers.graph.DirectedGraph;
 import com.github.nill14.parsers.graph.GraphEdge;
-import com.github.nill14.parsers.graph.utils.GraphWalker;
+import com.github.nill14.parsers.graph.utils.GraphWalker2;
 import com.github.nill14.parsers.graph.utils.LongestPathTopoSorter;
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
@@ -24,7 +23,7 @@ class DependencyGraph<M> implements IDependencyGraph<M> {
 	private final Set<M> modules;
 	private final DirectedGraph<M, GraphEdge<M>> graph;
 	private final LinkedHashMap<M, Integer> moduleRatings;
-	private final List<M> topologicalOrdering;
+	private final ImmutableList<M> topologicalOrdering;
 	
 	public DependencyGraph(DirectedGraph<M, GraphEdge<M>> graph) throws CyclicGraphException {
 		this.graph = graph;
@@ -66,43 +65,36 @@ class DependencyGraph<M> implements IDependencyGraph<M> {
 			final IConsumer<M> moduleConsumer)
 			throws ExecutionException {
 		
-		final GraphWalker<M> graphWalker = new GraphWalker<>(graph, topologicalOrdering);
-		
-		try {
-			for (final M module : graphWalker) {
-				executor.execute(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							moduleConsumer.process(module);
-							graphWalker.onComplete(module);
-						} catch (Exception e) {
-							graphWalker.onFailure(e);
-						}
+	final GraphWalker2<M> graphWalker = new GraphWalker2<>(graph, topologicalOrdering, moduleRatings);
+	
+		for (int i = 0; i < topologicalOrdering.size(); i++) {
+			final M module = graphWalker.take();
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						moduleConsumer.process(module);
+						graphWalker.onComplete(module);
+					} catch (Exception e) {
+						graphWalker.onFailure(e);
 					}
-				});
-			}
-			graphWalker.awaitCompletion();
-			
-		} catch (NoSuchElementException e) {
-			//see test GraphWalkerTest#testExhaustException
-			graphWalker.checkFailure();
+				}
+			});
 		}
+		graphWalker.awaitCompletion();
+			
 	}
 	
 	@Override
 	public void iterateTopoOrder(IConsumer<M> moduleConsumer) throws ExecutionException {
-		GraphWalker<M> graphWalker = new GraphWalker<>(graph, topologicalOrdering);
 		
-		for (M module : graphWalker) {
+		for (M module : topologicalOrdering) {
 			try {
 				moduleConsumer.process(module);
-				graphWalker.onComplete(module);
 			} catch (Exception e) {
 				throw new ExecutionException(e);
 			}
 		}
-		
 	}
 	
 }
